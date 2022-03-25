d4fbc59675523e548814122837f3772c77ae4d15
BatyshchevKirill <k.batyshchev@innopolis.university>
2022-03-25 18:16:20 +0300

    Update problem2.java

--- src/ru/innopolis/problem2/problem2.java
+++ src/ru/innopolis/problem2/problem2.java
@@ -1,2 +1,55 @@
-// your solution here
-//fix all the errors including package errors
+
+public class problem2 {
+    public static void main(String[] args) {
+        BurgerBuilder bb = new BurgerBuilder();
+        bb = bb.withCheese().withLetucci().withPeperone().withTomato().setSize("BIG");
+        bb.makeBurger().eatBurger();
+    }
+}
+class BurgerBuilder{
+    Burger burger;
+
+    public BurgerBuilder withCheese() {
+        this.burger.cheese = true;
+        return this;
+    }
+
+    public BurgerBuilder withPeperone() {
+        this.burger.peperone = true;
+        return this;
+    }
+
+    public BurgerBuilder withLetucci() {
+        this.burger.letucci = true;
+        return this;
+    }
+
+    public BurgerBuilder withTomato() {
+        this.burger.tomato = true;
+        return this;
+    }
+
+    public BurgerBuilder setSize(String Size) {
+        switch (Size) {
+            case "MINI" -> this.burger.size = SIZE.MINI;
+            case "MEDIUM" -> this.burger.size = SIZE.MEDIUM;
+            case "BIG" -> this.burger.size = SIZE.BIG;
+        }
+        return this;
+    }
+
+    Burger makeBurger(){
+        return this.burger;
+    }
+    class Burger{
+        boolean cheese, peperone, letucci, tomato;
+        SIZE size;
+        public void eatBurger(){
+            System.out.println("You are eating burger with " + (cheese? "cheese, ":"") + (peperone? "peperone, ":"") +
+                    (letucci? "letucci, ":"")+ (tomato? "tomato, ":"")+ "its size is "+ size );
+        }
+    }
+}
+enum SIZE{
+    MINI, MEDIUM, BIG;
+}
